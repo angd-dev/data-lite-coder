80bbf74ea0be6213cfe793ff03dacdab7a3f75ec
Oleksii Zghurskyi <oleksii.z@angd.dev>
2025-04-27 12:53:43 +0300

    Initial creation of Swift package

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DataLiteCoder",
+    platforms: [
+        .macOS(.v10_14),
+        .iOS(.v12)
+    ],
+    products: [
+        .library(name: "DataLiteCoder", targets: ["DataLiteCoder"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/angd-dev/data-lite-core.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+    ],
+    targets: [
+        .target(name: "DataLiteCoder"),
+        .testTarget(name: "DataLiteCoderTests", dependencies: ["DataLiteCoder"])
+    ]
+)
